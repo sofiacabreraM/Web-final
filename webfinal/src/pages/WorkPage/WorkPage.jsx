6eddb8df0654b1fa08c40b4bf92753f74f3f2a64
Carolina <carovel2608@gmail.com>
2024-05-02 09:30:42 -0500

    clean fields

--- webfinal/src/pages/WorkPage/WorkPage.jsx
+++ webfinal/src/pages/WorkPage/WorkPage.jsx
@@ -6,18 +6,50 @@ import { useState } from 'react';
 import vectorpink from '../../assets/vector-pink.png'
 
 export function Upload() {
-    const [ImageSelectedPrevious, setImageSelectedPrevious] = useState(null)
-    const changeImage = (e) => {
-      console.log(e.target.files);
-      if (e.target.files[0] !== undefined) {
-        const reader = new FileReader();
-        reader.readAsDataURL(e.target.files[0]);
-        reader.onload = (e) => {
-          e.preventDefault();
-          setImageSelectedPrevious(e.target.result);
-        };
-      }
+    const [title, setTitle] = useState('');
+    const [image, setImage] = useState(null)
+    const [description, setDescription] = useState('');
+    const [project, setProject] = useState('');
+    const [author, setAuthor] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSendClick = () => {
+        if (!title || !image || !description || !project || !author) {
+            setError('Please fill in all fields');
+            if (!title) setError('Please fill title');
+            if (!description) setError('Please fill description');
+            if (!image) setError('Please select an image');
+            if (!project) setError('Please select a type of proyect');
+            if (!author) setError('Please select an author');
+            return;
+        }
+
+        console.log("Title:", title);
+        console.log("Description:", description);
+        console.log("Image:", image);
+        console.log("Type of Project:", project);
+        console.log("Author:", author);
+        setError('');
+
+        setTitle('');
+        setDescription('');
+        setImage('');
+        setProject('');
+        setAuthor('');    
     };
+
+    const changeImage = (e) => {
+        console.log(e.target.files);
+        if (e.target.files[0] !== undefined) {
+          const reader = new FileReader();
+          reader.readAsDataURL(e.target.files[0]);
+          reader.onload = (e) => {
+            e.preventDefault();
+            setImage(e.target.result);
+          };
+        }
+      };
+
 
     return (
         <>
@@ -31,7 +63,7 @@ export function Upload() {
             <div className='upload-container'>
                 <div className='title'>
                     <h3>Title</h3>
-                    <input type='text' className='title-input'></input>
+                    <input type='text' className='title-input' value={title} onChange={(e) => setTitle(e.target.value)} required></input>
                 </div>
                 <div className='upload-rectangle'>
                     <div className='upload-text'>
@@ -53,9 +85,9 @@ export function Upload() {
                         />
                         </div>
                     </div>
-                    <div className="image-preview" style={{ display: ImageSelectedPrevious ? 'block' : 'none' }}>
+                    <div className="image-preview" style={{ display: image ? 'block' : 'none' }}>
                     <img
-                        src={ImageSelectedPrevious}
+                        src={image}
                         alt=""
                         height="150px"
                         width="250px"
@@ -74,22 +106,22 @@ export function Upload() {
                 </div>
                 <div className='description'>
                     <h3>Description</h3>
-                    <input type='text' className='description-input'></input>
+                    <input type='text' className='description-input' value={description} onChange={(e) => setDescription(e.target.value)} required></input>
                 </div>
                 <div className='menus'>
                     <div className='project-menu'>
                     <h4>Type of project</h4>
-                        <select className="project">
-                        <option value="" disabled selected hidden>Select an option</option>
-                            <option value="ux-ui">UX / UI Project</option>
-                            <option value="graphic">Graphic Project</option>
-                            <option value="brand">Brand Project</option>
-                        </select>
+                    <select className="project" value={project} onChange={(e) => setProject(e.target.value)} required>
+                        <option value="" disabled hidden>Select an option</option>
+                        <option value="ux-ui">UX / UI Project</option>
+                        <option value="graphic">Graphic Project</option>
+                        <option value="brand">Brand Project</option>
+                    </select>
                     </div>
                     <div className='author-menu'>
                         <h4>Author(s)</h4>
-                        <select className="author">
-                        <option value="" disabled selected hidden>Select an option</option>
+                        <select className="author" value={author} onChange={(e) => setAuthor(e.target.value)} required>
+                            <option value="" disabled hidden>Select an option</option>
                             <option value="caro">Carolina Velásquez</option>
                             <option value="lina">Lina Manjarrez</option>
                             <option value="pau">María Paula Ortiz</option>
@@ -97,22 +129,13 @@ export function Upload() {
                         </select>
                     </div>
                 </div>
+                {error && <div className="error-upload">{error}</div>}
                 <div className='buttons'>
                     <button className="draft-btn" href=" ">Save as draft</button>
-                    <button className="publish-btn" href=" ">Publish</button>
+                    <button className="publish-btn" onClick={() => { handleSendClick(); setImage(null); }}>Publish</button>
                 </div>
             </div>
             <Footer/>
         </>
     )
 }
-
-/*
-<label for="dog-names">Choose a dog name:</label>
-<select name="dog-names" id="dog-names">
-    <option value="rigatoni">Rigatoni</option>
-  <option value="dave">Dave</option>
-  <option value="pumpernickel">Pumpernickel</option>
-  <option value="reeses">Reeses</option>
-</select>
-*/
